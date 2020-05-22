e13caf5c17da5f7a9d91858b54e57d25b98fd473
Reid Ashwill <reidashwill@gmail.com>
2020-05-22 09:53:13 -0700

    being writing methods for volunteer class.  first two tests successfully passing as of now.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,9 +1,19 @@
 class Volunteer
   attr_reader :id
-  attr_accessor :name
-  
-  def initialize (attributes)
-    @name = attributes.fetch("name")
-    @id = attributes.fetch("id")
+  attr_accessor :name, :project_id
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+    @project_id = attributes.fetch(:project_id)
   end
+
+  def save
+    result = DB.exec("INSERT INTO volunteers (name) VALUES ('#{@name}'RETURNING id, name, project_id;")
+    @id = result.first().fetch("id").to_i
+    @name = result.first().fetch("name")
+    @project_id = result.first().fetch("project_id").to_i
+  end
+
+
 end
